31be21d342830afdffb145cffec1cd1cde6fa195
khulyso <khulyso.dev@gmail.com>
2023-08-15 16:04:13 +0200

    update

--- src/componets/About.jsx
+++ src/componets/About.jsx
@@ -4,8 +4,8 @@ import React from 'react'
   return (
     <div name='about' className='w-full h-screen bg-[#1E2D40] text-gray-300'>
         <div className='flex flex-col justify-center items-center w-full h-full'>
-            <div className='max-w-[1000px] w-f grid grid-cols-2 gap-8'>
-                <div className='sm:text-right pb-8 pl-4'>
+            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                <div className='sm:text-right py-8 pl-4'>
                     <p className='text-4xl font-bold inline border-b-4 border-[#00FFC3]'>
                         About
                     </p>
