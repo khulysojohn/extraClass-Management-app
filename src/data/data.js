24948a476e2d8da9643b116a5de08ba0accd714f
khulyso <khulyso.dev@gmail.com>
2024-07-23 15:58:25 +0200

    files updated

--- src/data/data.js
+++ src/data/data.js
@@ -1,47 +1,49 @@
 import WorkIm from '../assets/library.png'
 import System from '../assets/system.png'
+import calculator from '../assets/calculator.jpg'
+import color from '../assets/color.jpg'
 
 export const data=[
     {
         id:1,
         name:"Management System",
         image:System,
-        github:"",
+        github:"https://github.com/khulyso-dev/management-system",
         live:"",
     },
     {
         id:2,
-        name:"Smart Span",
-        image:WorkIm,
-        github:"",
+        name:"Calculator",
+        image:calculator,
+        github:"https://github.com/khulyso-dev/Calculator",
         live:"",
     },
     {
         id:3,
-        name:"Vendor Shop",
-        image:WorkIm,
-        github:"",
+        name:"Color Flipper",
+        image:color,
+        github:"https://github.com/khulyso-dev/color-flipper",
         live:"",
     },
     {
         id:4,
-        name:"Color Flipper",
+        name:"React app",
         image:WorkIm,
-        github:"",
+        github:"https://github.com/khulyso-dev/react-app",
         live:"",
     },
     {
         id:5,
-        name:"Basic Calculator",
+        name:"team webpage",
         image:WorkIm,
         github:"",
         live:"",
     },
     {
         id:6,
-        name:"React JS Application",
+        name:"Blog page",
         image:WorkIm,
-        github:"",
+        github:"https://github.com/khulyso-dev/Blog",
         live:"",
     },
 
