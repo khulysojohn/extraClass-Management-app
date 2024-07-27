0465e9bddb59d621e53cf14584ad7880381c1ec4
khulyso <khulyso.dev@gmail.com>
2024-07-27 14:18:29 +0200

    files updated

--- src/componets/Work.jsx
+++ src/componets/Work.jsx
@@ -36,13 +36,7 @@ const Work = () => {
         {item.name}
       </span>
       <div className="pt-8 text-center ">
-        {/* eslint-disable-next-line */}
-        <a href={item.github} target="_blank">
-          <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
-          >
-            Code
-          </button>
-        </a>
+       
         {/* eslint-disable-next-line */}
         <a href={item.live} target="_blank">
           <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"
